a217e29f6d9ea5db68e685c01957cec932eb55c0
dawid3201 <42513264+dawid3201@users.noreply.github.com>
2022-11-25 15:50:15 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Password dawidPassword = new Password();
-        String w = dawidPassword.randomPassword(16);
-        System.out.println(w);
-        dawidPassword.checkPassword(w);
+        Password newPassword = new Password();//create new password
+        String w = newPassword.randomPassword(16);//the password will contain 16 characters 
+        System.out.println(w);//print password
+        newPassword.checkPassword(w);//check if password contains at least 1 capital letter, 1 number and 1 special letter
 
 
     }
